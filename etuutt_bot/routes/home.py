42e8a631146739766c969d2398bd804253e5e294
Zalk0 <88582103+zalk0@users.noreply.github.com>
2024-02-26 15:11:19 +0100

    feat: get token to access student website API

--- etuutt_bot/routes/home.py
+++ etuutt_bot/routes/home.py
@@ -1,6 +1,10 @@
+from os import getenv
+
 from aiohttp import web
 
 
 async def handler(req: web.Request) -> web.Response:
-    # TODO
-    return web.Response(text="Home")
+    raise web.HTTPFound(
+        f"{getenv('API_URL')}/oauth/authorize"
+        f"?client_id={getenv('API_CLIENT_ID')}&response_type=code&state={getenv('API_STATE')}"
+    )
--- etuutt_bot/routes/login.py
+++ etuutt_bot/routes/login.py
@@ -3,4 +3,4 @@ from aiohttp import web
 
 async def handler(req: web.Request) -> web.Response:
     # TODO
-    return web.Response(text="Login")
+    return web.Response(text=f"Login successful\n{req.query.get('code')}")
