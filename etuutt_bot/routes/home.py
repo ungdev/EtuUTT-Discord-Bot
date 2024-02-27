720f6f516633b83a4f11d072c45cd1e00901c957
Zalk0 <88582103+zalk0@users.noreply.github.com>
2024-02-27 16:40:43 +0100

    feat: get token after getting code from user

--- etuutt_bot/routes/home.py
+++ etuutt_bot/routes/home.py
@@ -4,7 +4,7 @@ from aiohttp import web
 
 
 async def handler(req: web.Request) -> web.Response:
-    raise web.HTTPFound(
+    raise web.HTTPFound(  # HTTP 302
         f"{getenv('API_URL')}/oauth/authorize"
         f"?client_id={getenv('API_CLIENT_ID')}&response_type=code&state={getenv('API_STATE')}"
     )
--- etuutt_bot/routes/login.py
+++ etuutt_bot/routes/login.py
@@ -1,8 +1,24 @@
+from os import getenv
+
+import aiohttp
 from aiohttp import web
 
 
 async def handler(req: web.Request) -> web.Response:
-    # TODO
+    # Unauthorized if not code in query string
     if not req.query.get("code"):
-        return web.Response(status=401)
-    return web.Response(text=f"Login successful\n{req.query.get('code')}")
+        return web.HTTPUnauthorized()  # HTTP 401
+    # Request to obtain the access token
+    auth = aiohttp.BasicAuth(getenv("API_CLIENT_ID"), getenv("API_CLIENT_SECRET"))
+    async with aiohttp.ClientSession(auth=auth) as session:
+        data = {"grant_type": "authorization_code", "code": req.query.get("code")}
+        async with session.post(f"{getenv('API_URL')}/oauth/token", data=data) as response:
+            if not response.status == 200:
+                return web.Response(status=response.status)
+            resp = await response.json()
+            try:
+                token = resp["access_token"]
+            except KeyError:
+                return web.HTTPBadRequest()  # HTTP 400
+    # TODO: display form where user needs to enter discord username
+    return web.Response(text=f"Login successful\n{token}")  # Temp return
