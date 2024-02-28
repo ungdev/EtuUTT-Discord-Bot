c468ea4575a6b4def0e122ae4a9692ff928df3f6
Zalk0 <88582103+zalk0@users.noreply.github.com>
2024-02-28 13:53:28 +0100

    feat: add jinja template, define role route
    may put /role route back to GET

--- etuutt_bot/routes/login.py
+++ etuutt_bot/routes/login.py
@@ -1,6 +1,7 @@
 from os import getenv
 
 import aiohttp
+import aiohttp_jinja2
 from aiohttp import web
 
 
@@ -13,12 +14,11 @@ async def handler(req: web.Request) -> web.Response:
     async with aiohttp.ClientSession(auth=auth) as session:
         data = {"grant_type": "authorization_code", "code": req.query.get("code")}
         async with session.post(f"{getenv('API_URL')}/oauth/token", data=data) as response:
-            if not response.status == 200:
+            if response.status != 200:
                 return web.Response(status=response.status)
             resp = await response.json()
             try:
                 token = resp["access_token"]
             except KeyError:
                 return web.HTTPBadRequest()  # HTTP 400
-    # TODO: display form where user needs to enter discord username
-    return web.Response(text=f"Login successful\n{token}")  # Temp return
+    return await aiohttp_jinja2.render_template_async("form.html.jinja", req, {"token": token})
--- etuutt_bot/routes/role.py
+++ etuutt_bot/routes/role.py
@@ -1,6 +1,47 @@
+from os import getenv
+
+import aiohttp
+import aiohttp_jinja2
 from aiohttp import web
 
 
 async def handler(req: web.Request) -> web.Response:
-    # TODO
-    return web.Response(text="Role")
+    if req.method != "POST":
+        return web.HTTPMethodNotAllowed(req.method, ["POST"])  # HTTP 405
+    post = await req.post()
+    if "etu_token" and "discord_username" in post:
+        if post.get("check_GDPR") != "on":
+            return await aiohttp_jinja2.render_template_async(
+                "error.html.jinja",
+                req,
+                {
+                    "error": "Vous n'avez pas coché la case de consentement RGPD. "
+                    "Vos données n'ont pas été traitées. "
+                    "<a href='/'>Revenir au départ et recommencer !</a>"
+                },
+            )
+        async with aiohttp.ClientSession() as session:
+            params = {"access_token": post.get("etu_token")}
+            async with session.get(
+                f"{getenv('API_URL')}/public/user/account", params=params
+            ) as response:
+                if response.status != 200:
+                    return web.Response(status=response.status)
+                resp: dict = (await response.json()).get("data")
+                if all(  # Check if the fields we need are present
+                    field in resp
+                    for field in [
+                        "isStudent",
+                        "firstName",
+                        "lastName",
+                        "formation",
+                        "branch_list",
+                        "branch_level_list",
+                        "uvs",
+                    ]
+                ):
+                    return web.Response(
+                        text=f"{post.get('discord_username')}\n{resp.get('firstName')}"
+                    )
+                    # TODO: process information in another file
+    return web.HTTPBadRequest()
