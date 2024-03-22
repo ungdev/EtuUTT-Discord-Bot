4be8a07cf1cebaed58c4bb2d1bb6d04fb7685498
Zalko <88582103+Zalk0@users.noreply.github.com>
2024-03-22 09:53:15 +0100

    fix: CategoryChannel name does not correspond exactly to short name

--- etuutt_bot/commands/role.py
+++ etuutt_bot/commands/role.py
@@ -88,7 +88,11 @@ class Role(app_commands.Group):
     @app_commands.describe(category="La catégorie dans laquelle créer les salons")
     async def add_ues(self, interaction: Interaction[EtuUTTBot], category: CategoryChannel):
         await interaction.response.defer(thinking=True)
-        roles = (await parse_roles("roles.txt")).get(category.name)
+        if category.name.startswith("Master"):
+            cat = category.name.split(" ")[1]
+        else:
+            cat = category.name.split(" ")[0]
+        roles = (await parse_roles("roles.txt")).get(cat)
         if roles is None:
             await interaction.followup.send("Cette catégorie ne comporte aucune UE.")
             return
