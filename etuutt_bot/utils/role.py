c3fbd592710a8a4590d7ae17aca34da932e96b93
Zalk0 <88582103+zalk0@users.noreply.github.com>
2024-03-25 20:45:06 +0100

    fix: remove parse_categories because we don't need IDs anymore

--- etuutt_bot/utils/role.py
+++ etuutt_bot/utils/role.py
@@ -1,10 +1,6 @@
 from __future__ import annotations
 
-from functools import lru_cache
-from os import getenv
 from pathlib import PurePath
-
-from etuutt_bot.types import ChannelId
 
 
 def parse_roles(file: PurePath | str) -> dict[str, list[str]]:
@@ -14,27 +10,8 @@ def parse_roles(file: PurePath | str) -> dict[str, list[str]]:
     # content should look like "CS:MATH01,PHYS11,PC12;TM:NF05,TNEV"
 
     ues = {}
-    category_names = set(parse_categories().keys())
     for category in content.split(";"):
         cat_name, cat_ues = category.split(":")
-        if cat_name in category_names:
-            ues[cat_name] = [ue.strip() for ue in cat_ues.split(",")]
+        ues[cat_name] = [ue.strip() for ue in cat_ues.split(",")]
 
     return ues
-
-
-@lru_cache(maxsize=None)
-def parse_categories() -> dict[str, ChannelId]:
-    categories = getenv("UES_CATEGORIES")
-    if categories is None:
-        raise KeyError("Environment variable missing : UES_CATEGORIES")
-    result = {}
-    for cat in categories.split(","):
-        args = cat.split(":")
-        if len(args) != 2:
-            msg = f'UEs should be key/value pairs separated by a `:`. "{cat}" found instead'
-            raise ValueError(msg)
-        if not args[1].isdigit():
-            raise ValueError("Category IDs should be valid integers")
-        result[args[0]] = ChannelId(int(args[1]))
-    return result
