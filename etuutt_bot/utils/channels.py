d6092fae32c4e72d6a043fd36050344425a18095
Zalk0 <88582103+zalk0@users.noreply.github.com>
2024-03-28 15:14:21 +0100

    fix: better way to handle elected?

--- etuutt_bot/utils/channels.py
+++ etuutt_bot/utils/channels.py
@@ -1,16 +1,10 @@
-from __future__ import annotations
-
 from os import getenv
-from typing import TYPE_CHECKING
 
 from discord import CategoryChannel, PermissionOverwrite, Role, TextChannel
 
-if TYPE_CHECKING:
-    from etuutt_bot.bot import EtuUTTBot
-
 
 async def create_ue_channel(
-    bot: EtuUTTBot, category: CategoryChannel, role: Role, cat: str
+    category: CategoryChannel, role: Role, elected: Role = None
 ) -> TextChannel:
     guild = category.guild
     overwrites = {
@@ -18,8 +12,8 @@ async def create_ue_channel(
         role: PermissionOverwrite(read_messages=True),
         guild.get_role(int(getenv("MODERATOR_ID"))): PermissionOverwrite(read_messages=True),
     }
-    if elected := bot.data.get("Elected").get(cat):
-        overwrites.update({guild.get_role(elected): PermissionOverwrite(read_messages=True)})
+    if elected:
+        overwrites.update({elected: PermissionOverwrite(read_messages=True)})
     new_channel = await category.create_text_channel(role.name.lower(), overwrites=overwrites)
     await new_channel.send(f"Bonjour {role.mention}, votre salon textuel vient d'être créé !")
     return new_channel
