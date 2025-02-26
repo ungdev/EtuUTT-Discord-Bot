ac7518281138e730c3be6cf89d701b91754049bc
Zalk0 <88582103+zalk0@users.noreply.github.com>
2025-02-26 17:35:39 +0100

    fix: WIP waiting on site etu new oauth system

--- etuutt_bot/routes/home.py
+++ etuutt_bot/routes/home.py
@@ -6,6 +6,6 @@ from etuutt_bot.config import ApiConfig
 async def handler(req: web.Request) -> web.Response:
     api_settings: ApiConfig = req.app["bot"].settings.etu_api
     raise web.HTTPFound(  # HTTP 302
-        f"{api_settings.url}/auth/signin"
+        f"{api_settings.url}/auth/signin"  # Redirection sur le front du site étu
         # f"?client_id={api_settings.client_id}&response_type=code&state={req.app['api_state']}"  # noqa: ERA001
     )
