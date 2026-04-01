1c1a8a1ec7c5b15af51427cb17ce72dba2ff99f8
Zalk0 <88582103+zalk0@users.noreply.github.com>
2026-04-01 16:25:15 +0200

    chore: remove pin command as there is now a separate permission in Discord

--- etuutt_bot/commands/misc.py
+++ etuutt_bot/commands/misc.py
@@ -15,19 +15,13 @@ class MiscCog(commands.Cog):
     def __init__(self, bot: EtuUTTBot) -> None:
         self.bot = bot
         # Circumvent impossibility to add context menu commands in cogs
-        self.ctx_pin = app_commands.ContextMenu(
-            name="Épingler/Désépingler",
-            callback=self.pin,
-        )
         self.ctx_delete = app_commands.ContextMenu(
             name="Supprimer jusqu'ici",
             callback=self.delete,
         )
-        self.bot.tree.add_command(self.ctx_pin)
         self.bot.tree.add_command(self.ctx_delete)
 
     async def cog_unload(self) -> None:
-        self.bot.tree.remove_command(self.ctx_pin.name, type=self.ctx_pin.type)
         self.bot.tree.remove_command(self.ctx_delete.name, type=self.ctx_delete.type)
 
     # Simple ping command
@@ -36,21 +30,6 @@ class MiscCog(commands.Cog):
         await interaction.response.send_message(
             f"Pong! En {round(interaction.client.latency * 1000)}ms"
         )
-
-    # Make a simple context menu application to pin/unpin
-    @app_commands.guild_only
-    @app_commands.default_permissions(send_messages=True)
-    @app_commands.checks.has_permissions(send_messages=True)
-    @app_commands.checks.bot_has_permissions(manage_messages=True)
-    async def pin(self, interaction: discord.Interaction[EtuUTTBot], message: discord.Message):
-        if message.pinned:
-            await message.unpin()
-            await interaction.response.send_message(
-                "Le message a été désépinglé !", ephemeral=True
-            )
-        else:
-            await message.pin()
-            await interaction.response.send_message("Le message a été épinglé !", ephemeral=True)
 
     # Make a context menu command to delete messages
     @app_commands.guild_only
